12997e3c5480fe99e6eec09c8c22d095223b2e0e
blancabgz <blancabrilgonz@correo.ugr.es>
2020-12-23 00:57:31 +0100

    Ejercicio 3 y estructura del 5

---
+++ TEORIA/Retos/Reto_5/Ejercicio3/doc/html/search/functions_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['empty_43',['empty',['../classbintree.html#aefb9ea2b80770fec7cb1c0486740e25b',1,'bintree']]]
+];
---
+++ TEORIA/Retos/Reto_5/Ejercicio3/src/ejercicio.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include "bintree.h"
+
+using namespace std;
+
+
+
+  /* Arbol_1
+  //        2
+  //     /    
+  //    3
+  //  /  \
+  // 5    6
+  */
+
+
+int main() {
+  bool inferior;
+
+  bintree<int> arbol_1(2);
+  arbol_1.insert_left(arbol_1.root(), 3);
+  arbol_1.insert_right(arbol_1.root(), 4);
+  arbol_1.insert_left(arbol_1.root().left(), 5);
+  arbol_1.insert_right(arbol_1.root().left(), 6);
+  arbol_1.insert_left(arbol_1.root().right(), 7);
+
+
+
+
+  inferior = es_inferior(arbol_1, arbol_2);
+  if(inferior){
+    cout << "El arbol 1 es inferior al arbol 2" << endl;
+  }else{
+    cout << "El arbol 1 NO es inferior al arbol 2" << endl;
+  }
+}
