00870823aa06576d0fba70213d272aec3d6d1fe1
blancabgz <blancabrilgonz@correo.ugr.es>
2020-12-23 18:19:07 +0100

    Ejercicio 3 terminado

--- TEORIA/Retos/Reto_5/Ejercicio3/src/ejercicio.cpp
+++ TEORIA/Retos/Reto_5/Ejercicio3/src/ejercicio.cpp
@@ -4,26 +4,63 @@
 
 using namespace std;
 
+bool es_inferior(bintree<int> & ab1, bintree<int> & ab2){
+  vector<int> arbol_1;
+  vector<int> arbol_2;
+
+  if(ab1.size() == ab2.size()){
+
+    for (bintree<int>::preorder_iterator i = ab1.begin_preorder(); i!=ab1.end_preorder(); ++i){
+      arbol_1.push_back(*i);
+    }
+
+    for(bintree<int>::preorder_iterator i = ab2.begin_preorder(); i!=ab2.end_preorder(); ++i){
+      arbol_2.push_back(*i);
+    }
+
+    for( unsigned int i = 0; i < ab1.size(); i++){
+      if(arbol_1[i] > arbol_2[i]){
+        return false;
+      }
+    }
+    return true;
+
+  }else{
+    return false;
+  }
+
+}
 
 
-  /* Arbol
-  //        2
-  //     /
-  //    3
-  //  /  \
-  // 5    6
-  */
+/* creo un arbol_2:           creo un arbol_1:
+//        2                          1
+//     /    \                      /   \
+//    3     4                     2    3
+//  /  \    / \                 /  \  / \
+// 5    6  7  8                4   5 6   7
 
+*/
 
 int main() {
   bool inferior;
 
-  bintree<int> arbol_1(2);
-  arbol_1.insert_left(arbol_1.root(), 3);
-  arbol_1.insert_right(arbol_1.root(), 4);
-  arbol_1.insert_left(arbol_1.root().left(), 5);
-  arbol_1.insert_right(arbol_1.root().left(), 6);
-  // arbol_1.insert_left(arbol_1.root().right(), 7);
+  bintree<int> arbol_1(1);
+  arbol_1.insert_left(arbol_1.root(), 2);
+  arbol_1.insert_right(arbol_1.root(), 3);
+  arbol_1.insert_left(arbol_1.root().left(), 4);
+  arbol_1.insert_right(arbol_1.root().left(), 5);
+  arbol_1.insert_left(arbol_1.root().right(), 6);
+  arbol_1.insert_right(arbol_1.root().right(), 7);
+
+  bintree<int> arbol_2(2);
+  arbol_2.insert_left(arbol_2.root(), 3);
+  arbol_2.insert_right(arbol_2.root(), 4);
+  arbol_2.insert_left(arbol_2.root().left(), 5);
+  arbol_2.insert_right(arbol_2.root().left(), 6);
+  arbol_2.insert_left(arbol_2.root().right(), 7);
+  arbol_2.insert_right(arbol_2.root().right(), 8);
+
+
 
   inferior = es_inferior(arbol_1, arbol_2);
   if(inferior){
