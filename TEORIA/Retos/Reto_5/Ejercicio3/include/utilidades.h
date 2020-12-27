d9d80bdb37fbebc55458c85d418baa0d2d9a4686
blancabgz <blancabrilgonz@correo.ugr.es>
2020-12-27 17:38:41 +0100

    comentado ejercicio 3

---
+++ TEORIA/Retos/Reto_5/Ejercicio3/include/utilidades.h
@@ -0,0 +1,17 @@
+#ifndef UTILIDADES_H
+#define UTILIDADES_H
+#include <vector>
+#include "bintree.h"
+using namespace std;
+
+/**
+* @brief Mira si un arbol es inferior a otro
+* @param ab1 primer arbol
+* @param ab2 segundo arbol
+* @result si ab1 es menor que ab2 devuelve true
+* @author Blanca Abril
+*/
+
+bool es_inferior(bintree<int> & ab1, bintree<int> & ab2);
+
+#endif
--- TEORIA/Retos/Reto_5/Ejercicio3/src/ejercicio.cpp
+++ TEORIA/Retos/Reto_5/Ejercicio3/src/ejercicio.cpp
@@ -1,35 +1,9 @@
 #include <iostream>
 #include <vector>
 #include "bintree.h"
+#include "utilidades.h"
 
 using namespace std;
-
-bool es_inferior(bintree<int> & ab1, bintree<int> & ab2){
-  vector<int> arbol_1;
-  vector<int> arbol_2;
-
-  if(ab1.size() == ab2.size()){
-
-    for (bintree<int>::preorder_iterator i = ab1.begin_preorder(); i!=ab1.end_preorder(); ++i){
-      arbol_1.push_back(*i);
-    }
-
-    for(bintree<int>::preorder_iterator i = ab2.begin_preorder(); i!=ab2.end_preorder(); ++i){
-      arbol_2.push_back(*i);
-    }
-
-    for( unsigned int i = 0; i < ab1.size(); i++){
-      if(arbol_1[i] > arbol_2[i]){
-        return false;
-      }
-    }
-    return true;
-
-  }else{
-    return false;
-  }
-
-}
 
 
 /* creo un arbol_2:           creo un arbol_1:
